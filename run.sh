824802a529128442ff13ae5214817a6963ce290a
gena460 <gena460@gmail.com>
2026-02-04 22:34:28 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-# Usage: ./run.sh [logfile] [csvfile] [options]
-python3 convert_nginx_log.py "$@"
+set -e
 
-# Add CSV file to git and commit
-csvfile="$2"
-git add "$csvfile"
-git commit -m "Add CSV export from nginx log: $csvfile"
+python3 convert_nginx_log.py "$@"
+git add "$2"
+git commit -m "Add/Update CSV nginx log export: $2"
